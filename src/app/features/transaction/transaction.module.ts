29fc5f1ecd9bd42afc38f71a7a407694aa4fe0a0
marouaneXan <marouanezahaoui835@gmail.com>
2023-04-06 14:08:06 +0000

    generate a add-transaction component

--- src/app/features/transaction/transaction.module.ts
+++ src/app/features/transaction/transaction.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListTransactionsComponent } from './components/list-transactions/list-transactions.component';
+import { AddTransactionComponent } from './components/add-transaction/add-transaction.component';
 
 
 
 @NgModule({
   declarations: [
-    ListTransactionsComponent
+    ListTransactionsComponent,
+    AddTransactionComponent
   ],
   imports: [
     CommonModule
