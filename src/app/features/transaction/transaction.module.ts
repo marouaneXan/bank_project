d3aa15d98fe6c3612f45db86b970c06efd275588
marouaneXan <marouanezahaoui835@gmail.com>
2023-04-06 14:31:38 +0000

    generate module for transactions page + add navbar to list-transactions

--- src/app/features/transaction/transaction.module.ts
+++ src/app/features/transaction/transaction.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListTransactionsComponent } from './components/list-transactions/list-transactions.component';
 import { AddTransactionComponent } from './components/add-transaction/add-transaction.component';
+import { PartialsModule } from 'src/app/views/partials/partials.module';
 
 
 
@@ -11,7 +12,8 @@ import { AddTransactionComponent } from './components/add-transaction/add-transa
     AddTransactionComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    PartialsModule
   ]
 })
 export class TransactionModule { }
