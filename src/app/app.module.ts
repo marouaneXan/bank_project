c8eb15d0c7f64697eaa57e56bbf7adc399a57f50
marouaneXan <marouanezahaoui835@gmail.com>
2023-04-06 13:52:50 +0000

    add router outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AuthModule } from './features/auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +10,7 @@ import { AuthModule } from './features/auth/auth.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
