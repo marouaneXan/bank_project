146d63df9014c4ad2cbba2b85c1a4715a3f4d9a9
marouaneXan <marouanezahaoui835@gmail.com>
2023-04-16 15:07:52 +0000

    disable access to devTools

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,21 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+// disable access to devTools
+document.onkeydown = (e: any) => {
+  if (e.key == 123) {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.key == 'U') {
+    e.preventDefault();
+  }
+};
