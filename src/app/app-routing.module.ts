dd2fed6d64868bfdbeca95036e9861e79070524c
oumaima <oumaimaabouelhaitam@gmail.com>
2023-04-17 03:00:03 +0000

    merge list-transaction && main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
     },
     {
         path: 'admin',
-        // canActivate: [AuthActivateGuard],
+        canActivate: [AuthActivateGuard],
         children: [
             { path: '', loadChildren: () => import('./layouts/layouts.module').then(m => m.LayoutsModule) }
         ],
