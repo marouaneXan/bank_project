e06332d5002c7fa6c3185adc868c1f8e6138dfb6
marouaneXan <marouanezahaoui835@gmail.com>
2023-04-18 10:56:28 +0000

    generate delete-transaction component

---
+++ src/app/features/transaction/components/delete-transaction/delete-transaction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-transaction',
+  templateUrl: './delete-transaction.component.html',
+  styleUrls: ['./delete-transaction.component.css']
+})
+export class DeleteTransactionComponent {
+
+}
