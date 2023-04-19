d14b7f06a41750dfdc9df635983d840b246ccb0e
oumaima <oumaimaabouelhaitam@gmail.com>
2023-04-19 11:42:38 +0000

    termine modal

--- src/app/features/global-modal/global-modal.component.ts
+++ src/app/features/global-modal/global-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input  } from '@angular/core';
 
 @Component({
   selector: 'app-global-modal',
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./global-modal.component.css']
 })
 export class GlobalModalComponent {
+  @Input() sourceAccount: string="";
+  @Input() destinationAccount: string="";  
+  @Input() amount: string="";
+  @Input() agentId: string="";
+  @Input() title: string="";
   showModal = false;
   toggleModal(){
     this.showModal = !this.showModal;
