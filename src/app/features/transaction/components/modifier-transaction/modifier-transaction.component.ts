ae2c1a52d81fcac409ad9a0d6e94ac4d5a63982b
oumaima <oumaimaabouelhaitam@gmail.com>
2023-04-20 13:31:40 +0000

    add function updateTransaction && add form editeTransaction

--- src/app/features/transaction/components/modifier-transaction/modifier-transaction.component.ts
+++ src/app/features/transaction/components/modifier-transaction/modifier-transaction.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TransactionService } from '../../services/transaction.service';
+import { ToastrService } from 'ngx-toastr';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modifier-transaction',
   templateUrl: './modifier-transaction.component.html',
-  styleUrls: ['./modifier-transaction.component.css']
+  styleUrls: ['./modifier-transaction.component.css'],
 })
 export class ModifierTransactionComponent {
+  @Input() title: string = '';
+  @Input() transactionData: any = [];
+  showModal = false;
+  toggleModal(){
+    this.showModal = false;
+  }
+  editform!: FormGroup;
 
+  constructor(
+    private transactionService: TransactionService,
+    private toastr: ToastrService
+  ) {}
+  ngOnInit(): void {
+    this.editform = new FormGroup({
+      sourceAccount: new FormControl('', [Validators.required]),
+      destinationAccount: new FormControl('', [Validators.required]),
+      amount: new FormControl("", [Validators.required]),
+      fromId: new FormControl("", [Validators.required]),
+      toId: new FormControl("", [Validators.required]),
+    });
+        console.log(this.editform.value)
+
+  }
+  get f(){
+    return this.editform.controls;
+  }
+  submit(){
+
+try {
+
+  this.transactionService.updateTransaction(this.transactionData.id,this.editform.value)?console.log("Post updated successfully!"):console.log("error")
+  this.toggleModal()
+} catch (error) {
+  console.log(error)
+}
+
+
+}
 }
