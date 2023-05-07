abf577b5ae1e9a7949f122640f8a66195f0e0585
marouane zahaoui <marouanezahaoui835@gmail.com>
2023-05-07 18:54:39 +0100

    bugs: fix activate links sidebar

--- src/app/views/partials/sidebar/sidebar.component.ts
+++ src/app/views/partials/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,28 +12,28 @@ export class SidebarComponent implements OnInit {
   isMenu: boolean = false;
   isSearch: boolean = false;
   activeLinkIndex: number = 0;
-  constructor() { }
+  constructor(private router: Router) { }
 
   sidebarLinks = [
     {
       path: '/admin',
       name: 'Dashboard',
-      icon:'fa-solid fa-gauge'
+      icon: 'fa-solid fa-gauge'
     },
     {
       path: '/admin/transactions',
       name: 'Transactions',
-      icon:'fa-solid fa-credit-card'
+      icon: 'fa-solid fa-credit-card'
     },
     {
       path: '/admin/transactions/create',
       name: 'Add Transaction',
-      icon:'fa-solid fa-person-circle-check'
+      icon: 'fa-solid fa-person-circle-check'
     },
     {
       path: '/admin/profile',
       name: 'Profile',
-      icon:'fa-solid fa-user'
+      icon: 'fa-solid fa-user'
     },
   ]
   ngOnInit() {
@@ -39,11 +41,33 @@ export class SidebarComponent implements OnInit {
     if (activeLinkIndex) {
       this.activeLinkIndex = +activeLinkIndex;
     }
+
+    this.setActiveLinkIndex();
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.setActiveLinkIndex();
+      });
   }
+
+
 
   setActiveLink(index: number) {
     this.activeLinkIndex = index;
-    console.log('index:', this.activeLinkIndex);
-    localStorage.setItem('activeLinkIndex', String(index));
+    localStorage.setItem('activeLinkIndex', String(this.activeLinkIndex));
   }
+  setActiveLinkByUrl(url: string) {
+    const index = this.sidebarLinks.findIndex((link) => url.includes(link.path));
+    if (index !== -1) {
+      this.setActiveLink(index);
+    }
+  }
+  setActiveLinkIndex() {
+    const activeIndex = this.sidebarLinks.findIndex((link) => this.router.isActive(link.path, true));
+    if (activeIndex !== -1) {
+      this.activeLinkIndex = activeIndex;
+    }
+  }
+
 }
