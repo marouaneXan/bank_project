e2bbd5baf198aca532c95e3b70a93d883473d60c
marouane zahaoui <marouanezahaoui835@gmail.com>
2023-05-07 12:18:12 +0100

    feat+bugs: generate new access_token if his expired

--- src/app/core/interceptor/protection.interceptor.ts
+++ src/app/core/interceptor/protection.interceptor.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse, HttpEvent } from '@angular/common/http';
 import { Observable, throwError, BehaviorSubject, from } from 'rxjs';
-import { catchError, filter, switchMap, take, retry } from 'rxjs/operators';
+import { catchError, filter, switchMap, take } from 'rxjs/operators';
 import { OAuthService } from 'angular-oauth2-oidc';
 import { TokenService } from '../services/token.service';
 
@@ -26,9 +26,7 @@ export class ProtectionInterceptor implements HttpInterceptor {
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
         console.log(error);
-        if (error.status === 403 && error.statusText === "Forbidden") {
-          return this.handle403Error(request, next).pipe(retry(3));
-        } else if (error.status === 401 && error.statusText === "Unauthorized") {
+        if (error.status === 401 && error.statusText === "Unauthorized") {
           return this.handle401Error(request, next);
         }
         return throwError(error);
@@ -36,31 +34,27 @@ export class ProtectionInterceptor implements HttpInterceptor {
     );
   }
 
-  private handle403Error(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const accessToken = this.oauthService.getAccessToken();
-    request = request.clone({
-      setHeaders: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    });
-    return next.handle(request);
-  }
-
   private handle401Error(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (!this.refreshTokenInProgress) {
       this.refreshTokenInProgress = true;
       this.refreshTokenSubject.next(null);
+
       return from(this.oauthService.refreshToken()).pipe(
         switchMap(() => {
           this.refreshTokenInProgress = false;
-          this.refreshTokenSubject.next(true);
-          const accessToken = this.oauthService.getAccessToken();
-          request = request.clone({
-            setHeaders: {
-              Authorization: `Bearer ${accessToken}`
-            }
-          });
-          return next.handle(request);
+          this.refreshTokenSubject.next(this.oauthService.getAccessToken());
+          return this.refreshTokenSubject.pipe(
+            filter(token => token !== null),
+            take(1),
+            switchMap(token => {
+              request = request.clone({
+                setHeaders: {
+                  Authorization: `Bearer ${token}`
+                }
+              });
+              return next.handle(request);
+            })
+          );
         }),
         catchError((error: HttpErrorResponse) => {
           this.refreshTokenInProgress = false;
@@ -70,13 +64,12 @@ export class ProtectionInterceptor implements HttpInterceptor {
       );
     } else {
       return this.refreshTokenSubject.pipe(
-        filter(result => result !== null),
+        filter(token => token !== null),
         take(1),
-        switchMap(() => {
-          const accessToken = this.oauthService.getAccessToken();
+        switchMap(token => {
           request = request.clone({
             setHeaders: {
-              Authorization: `Bearer ${accessToken}`
+              Authorization: `Bearer ${token}`
             }
           });
           return next.handle(request);
